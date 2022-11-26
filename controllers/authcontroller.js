b64222f780709ccbc0da2804b2c6437aade5a16f
Yashdeep singh kushwaha <104566080+Kushyashsingh@users.noreply.github.com>
2022-11-26 22:05:02 +0530

    update

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -84,22 +84,35 @@ module.exports.login_post = async (req, res) => {
 }
 
 module.exports.reset_post = async(req,res)=>{
-  const {email}=req.body;
-  //user exist or not
-  if(email !== User.email){
-    res.send('User not registered')
-  }
+  User.findOne({email: req.body.email})
+        .then(user => {
+            if (!user) return res.status(401).json({message: 'The email address ' + req.body.email + ' is not associated with any account. Double-check your email address and try again.'});
 
-  //user exist and create one time link
-  const secret = JWT_SECRET+ User.password
-  const payload = {
-    email:User.email,
-    id: user.id,
-  }
-  const token = jwt.sign(payload, secret , {expiresIn: '15m'})
+            //Generate and set password reset token
+            user.generatePasswordReset();
 
-  const link = `http://localhost:9000/password/reset/${token}`
-  console.log(link)
-  res.send('Password reset link has been sent to your email')
+            // Save the updated user object
+            user.save()
+                .then(user => {
+                    // send email
+                    let link = "http://" + req.headers.host + "/api/auth/reset/" + user.resetPasswordToken;
+                    const mailOptions = {
+                        to: user.email,
+                        from: process.env.FROM_EMAIL,
+                        subject: "Password change request",
+                        text: `Hi ${User.email} \n 
+                    Please click on the following link ${link} to reset your password. \n\n 
+                    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+                    };
+
+                    sgMail.send(mailOptions, (error, result) => {
+                        if (error) return res.status(500).json({message: error.message});
+
+                        res.status(200).json({message: 'A reset email has been sent to ' + user.email + '.'});
+                    });
+                })
+                .catch(err => res.status(500).json({message: err.message}));
+        })
+        .catch(err => res.status(500).json({message: err.message}));
 }
 
